e5afca0c2a167280a672f69d830cc90804e32eeb
rehmet yeshanew <ryfgmz87@gmail.com>
2023-12-30 19:04:41 +0300

    chat copy

--- client/app/components/chat/ChatBox.jsx
+++ client/app/components/chat/ChatBox.jsx
@@ -6,7 +6,8 @@ import Chat from '@/services/chat';
 
 export default function ChatBox() {
   const [input, setInput] = useState("")
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
   const [chatHistory, setChatHistory] = useState([]);
 
   const handler = (event) => {
@@ -36,6 +37,60 @@ export default function ChatBox() {
      setChatHistory((history) => [...history, data]);
      setInput("");
   }
+
+  const handleCopy = (xx) => {
+    copy(xx);
+    setIsCopied(true);
+  };
+
+  const formatingCode = (code) => {
+    // Split the code by lines to remove unnecessary white spaces
+    const lines = code.split(/\s*(?=<)/);
+    
+    // Remove leading and trailing white spaces from each line
+    const trimmedLines = lines.map((line) => line.trim());
+    
+    // Join the lines with line breaks and indentation
+    const formattedCode = trimmedLines.join('\n');
+    
+    return formattedCode;
+  };
+
+  const splitAnswerParts = (answer) => {
+    const parts = [];
+    const codeRegex = /```([\s\S]*?)```/g;
+  
+    let match;
+    let lastIndex = 0;
+  
+    while ((match = codeRegex.exec(answer))) {
+      const codeContent = match[1].trim();
+  
+      if (match.index > lastIndex) {
+        const textContent = answer.substring(lastIndex, match.index).trim();
+        parts.push({ type: 'text', content: textContent });
+      }
+  
+      const lines = codeContent.split('\n');
+      const firstLine = lines[0].trim();
+      const firstWord = firstLine.split(' ')[0];
+      const firstLineEndIndex = codeContent.indexOf(' ') + 1;
+      const remainingCode = codeContent.substring(firstLineEndIndex).trim();
+      const formattedCodeContent = formatingCode(remainingCode); // Process the remaining code through formatingCode function
+  
+      parts.push({ type: 'code', firstWord, content: formattedCodeContent });
+  
+      lastIndex = match.index + match[0].length;
+    }
+  
+    if (lastIndex < answer.length) {
+      const textContent = answer.substring(lastIndex).trim();
+      parts.push({ type: 'text', content: textContent });
+    }
+  
+    return parts;
+  };
+
  
 
   return (
@@ -49,27 +104,60 @@ export default function ChatBox() {
 
             <div className='chatbox'>
               {chatHistory.map((chat, index) => (
-                  <div key={index} className="chatcontain ">
-                    {chat.sender === "user" ? (
-                      <>
-                        <div className="user">
-                          <div className="">
-                            <p className="parauser">{chat?.text}</p>
+                <div key={index} className="chatcontain">
+                  {chat.sender === "user" ? (
+                    <div className="user">
+                      <div className="">
+                        <p className="parauser">{chat?.text}</p>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className='ai'>
+                      <div className="">
+                        {chat?.text && (
+                          <div>
+                            {splitAnswerParts(chat.text).map((part, partIndex) => (
+                              <React.Fragment key={partIndex}>
+                                {part.type === 'code' ? (
+                                  <div className="">
+                                    <div className='chat-head'>
+                                        <div className='copy' onClick={() => handleCopy(part.content)}>
+                                          {isCopied ? <FaCheck className='check'/> : <IoCopy className='copyicon'/>}
+                                        </div>
+                                        <div className=''>
+                                          {part.firstWord}
+                                        </div>                            
+                                    </div>
+                                    <SyntaxHighlighter
+                                      language={part.firstWord}
+                                      style={docco}
+                                      className='x-container'
+                                      customStyle={{
+                                        fontSize: '14px',
+                                        lineHeight: '1.5',
+                                        maxWidth: '100%',
+                                        width: '24.7rem',
+                                        wordBreak: 'break-word',
+                                        overflowWrap: 'break-word',
+                                        whiteSpace: 'pre-wrap',
+                                        overflow: 'auto'
+                                      }}
+                                    >
+                                      {part.content}
+                                    </SyntaxHighlighter>
+                                  </div>
+                                ) : (
+                                  <p>{part.content}</p>
+                                )}
+                              </React.Fragment>
+                            ))}
                           </div>
-                        </div>
-                
-                      </>
-                    ) : (
-                      <>
-                        <div className='ai'>
-                            <div className="">
-                              <p className="parai">{chat?.text}</p>
-                            </div>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                ))}
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
 
             <div className='inputbox'>        
